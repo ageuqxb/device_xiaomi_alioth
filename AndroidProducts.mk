a9a3e46853c17c548521224af014b05e3add21c0
AgeuQXB <nonoqxb@gmail.com>
2022-06-13 23:58:51 -0300

    AndroidProducts: Remove lunch target

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,2 @@
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/flamingo_alioth \
-
-# Lunch targets
-COMMON_LUNCH_CHOICES += \
-    flamingo_alioth-user \
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/flamingo_alioth.mk
